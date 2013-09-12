816d1e09a5287bf2a95a92aa2764a99d1851f259
Chris <chris@cbaus.de>
2013-09-12 04:05:51 +0200

    [FIX] dont send mail if user set mail send flag to false
    works only with issue and news class
    
    see
    
    app/models/user.rb : notify_about?(object)

--- lib/redshare_observer.rb
+++ lib/redshare_observer.rb
@@ -1,7 +1,7 @@
 class RedshareObserver < ActiveRecord::Observer
   def after_create(redshare)
     if redshare
-      Mailer.redshare_add(redshare).deliver
+      Mailer.redshare_add(redshare).deliver if redshare.user.notify_about?(redshare)
     end
   end
 end
