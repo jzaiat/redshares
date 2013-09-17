acddba6ad7e7d83b5272f8c515f08a449e28e3a8
Chris <chris@cbaus.de>
2013-09-17 23:23:12 +0200

    bugfix redshare observer

--- lib/redshare_observer.rb
+++ lib/redshare_observer.rb
@@ -1,7 +1,7 @@
 class RedshareObserver < ActiveRecord::Observer
   def after_create(redshare)
     if redshare
-      Mailer.redshare_add(redshare).deliver if redshare.user.notify_about?(redshare)
+      Mailer.redshare_add(redshare).deliver unless redshare.user.mail_notification.blank? || redshare.user.mail_notification == 'none'
     end
   end
 end
